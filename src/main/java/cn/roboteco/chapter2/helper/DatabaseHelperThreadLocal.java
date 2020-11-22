d6b65c7dc7a8dc5e05410141b5671013f1258517
wanghao0911 <1011954307@qq.com>
2020-11-22 19:59:19 +0800

    dbutils

--- src/main/java/cn/roboteco/chapter2/helper/DatabaseHelperThreadLocal.java
+++ src/main/java/cn/roboteco/chapter2/helper/DatabaseHelperThreadLocal.java
@@ -1,16 +1,21 @@
 package cn.roboteco.chapter2.helper;
 
+import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.exceptions.ExceptionUtil;
 import cn.roboteco.chapter2.utils.PropUtil;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 public class DatabaseHelperThreadLocal {
@@ -80,5 +85,99 @@ public class DatabaseHelperThreadLocal {
             closeConnection();
         }
         return entityList;
+    }
+
+    public static <T> T queryEntity(Class<T> entityClass, String sql, Object... params){
+        T entity;
+        try {
+            entity = QUERY_RUNNER.query(getConnection(), sql, new BeanHandler<T>(entityClass), params);
+        } catch (Exception e){
+            logger.error(ExceptionUtil.stacktraceToString(e));
+            throw new RuntimeException(e);
+        } finally {
+            closeConnection();
+        }
+        return entity;
+    }
+
+    /**
+     * Map表示列名与列值的映射关系
+     *
+     * @param sql
+     * @param params
+     * @return
+     */
+    public static List<Map<String, Object>> executeQuery(String sql, Object... params){
+        List<Map<String, Object>> result;
+        try {
+            result = QUERY_RUNNER.query(getConnection(), sql, new MapListHandler(), params);
+        } catch (Exception e){
+            logger.error(ExceptionUtil.stacktraceToString(e));
+            throw new RuntimeException(e);
+        }
+        return result;
+
+    }
+
+    /**
+     * 执行更新（包括insert、delete、update）
+     * @param sql
+     * @param params
+     * @return 返回执行结果的行数
+     */
+    public static int executeUpdate(String sql, Object... params){
+        int rows = 0;
+        try {
+            rows = QUERY_RUNNER.update(getConnection(), sql, params);
+        } catch (Exception e){
+            logger.error(ExceptionUtil.stacktraceToString(e));
+            throw new RuntimeException(e);
+        }
+        return rows;
+    }
+
+    public static <T> boolean insertEntity(Class<T> entityClass, Map<String,Object> fieldMap){
+        if(CollectionUtil.isEmpty(fieldMap)){
+            logger.error("fieldMap is null");
+            return false;
+        }
+        String sql = "insert into " + getTableName(entityClass);
+        StringBuilder columns = new StringBuilder("(");
+        StringBuilder values = new StringBuilder("(");
+        for (String fieldName:fieldMap.keySet()){
+            columns.append(fieldName).append(",");
+            values.append("?,");
+        }
+        columns.replace(columns.lastIndexOf(","),columns.length(),")");
+        values.replace(values.lastIndexOf(","), values.length(),")");
+        sql+=columns+" values " + values;
+        Object[] params = fieldMap.values().toArray();
+        return executeUpdate(sql, params) == 1;
+    }
+
+    public static <T> boolean updateEntity(Class<T> entityClass, long id, Map<String, Object> fieldMap){
+        if(CollectionUtil.isEmpty(fieldMap)){
+            logger.error("fieldMap is null");
+            return false;
+        }
+        String sql = "update " + getTableName(entityClass) + " set ";
+        StringBuilder columns = new StringBuilder();
+        for (String fieldName:fieldMap.keySet()){
+            columns.append(fieldName).append("=?,");
+        }
+        sql += columns.substring(0,columns.lastIndexOf(",")) + " where id=?";
+        List<Object> paramList = new ArrayList<>();
+        paramList.addAll(fieldMap.values());
+        paramList.add(id);
+        Object[] params = paramList.toArray();
+        return executeUpdate(sql,params) == 1;
+    }
+
+    public static <T> boolean deleteEntity(Class<T> entityClass, long id){
+        String sql = "delete from "+ getTableName(entityClass) + " where id = ?";
+        return executeUpdate(sql,id) == 1;
+    }
+    private static String getTableName(Class<?> entityClass){
+        return entityClass.getSimpleName();
     }
 }
